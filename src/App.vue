ca57d292fca97af2dc16052043eae38b9aef6a17
Asgarov Asgar <asgara@maestro.st>
2023-04-23 23:45:06 +0400

    watch, watch effect

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,50 @@
+<template>
+  <h1>{{ name }}</h1>
+  <button @click="removeWatcher">Remove watcher</button>
+  <button @click="placeOrder">Place Order</button>
+  <YummyMeal v-for="meal in meals" :key="meal.name" :name="meal.name" :price="meal.price" @addToCart="addItemToCart" :currencySymbol="currencySymbol" />
+</template>
+
 <script>
+import YummyMeal from './components/YummyMeal.vue'
+import { ref, reactive, watch } from 'vue'
 
 export default {
+  components: { YummyMeal },
   setup() {
-    const name = 'Snazzy burger'
-    return {name}
+    const name = ref('Snazzy burger')
+    const currencySymbol = '$'
+    const cart = reactive([])
+    const meal = reactive({ name: 'Hamburger', price: 5 })
+    const meals = reactive([
+    { name: 'Hamburger', price: 5 },
+    { name: 'Cheeseburger', price: 6 },
+    { name: 'Impossible burger', price: 7 },
+    { name: 'Fries', price: 2 },
+    ])
+    const placeOrder = () => alert('your order has been placed!')
+    const addItemToCart = (item) => cart.push(item)
+    // watch(() => [...cart], (newName, oldName) => console.log(newName, oldName))
+    // const removeWatcher = watch([name, () => [...cart]], (newName, oldName) => console.log(newName, oldName))
+    const removeWatcher = watch([() => [...cart]], (newName, oldName) => alert(`${newName} added to the cart`))
+    return { name, placeOrder, addItemToCart, meal, meals, removeWatcher, currencySymbol }
   }
 }
 </script>
 
-<template>
-<h1>{{ name }}</h1>
- 
-</template>
-
-<style scoped>
+<!-- <style scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
-</style>
+</style> -->
---
+++ src/components/YummyMealPrice.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>{{ pricePretty }}</p>
+</template>
+
+<script>
+import { computed } from 'vue';
+export default {
+  props: {
+    currencySymbol:String,
+    price: Number
+  },
+  setup(props) {
+    const pricePretty = computed(() => {
+      return `${props.currencySymbol}${props.price}`
+    })
+
+    return { pricePretty}
+  }
+}
+
+</script>
+
+<style scoped>
+.yummy {
+  display: flex;
+  gap: 8px;
+  margin: 4px;
+}
+</style>
